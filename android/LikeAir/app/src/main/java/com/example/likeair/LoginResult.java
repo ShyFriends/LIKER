9bb62ccfde6d3dbd7f76032a5f4b9dfee9329200
ParkSanjun <qpfjdksl@gmail.com>
2020-01-29 15:04:51 -0800

    liker login activity

---
+++ android/LikeAir/app/src/main/java/com/example/likeair/LoginResult.java
@@ -0,0 +1,16 @@
+package com.example.likeair;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginResult extends AppCompatActivity {
+
+    TextView textView_LikeAir;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ android/LikeAir/app/src/main/java/com/example/likeair/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.likeair;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText EditText_login, EditText_password;
+    Button Button_login;
+    TextView TextView_forgot, TextView_creat;
+
+
+    TextView textView_LikeAir;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
