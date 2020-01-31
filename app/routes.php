f35e06601f6fa4b4425e59ff5f6fa3655d158aa2
JangHyukJin <wkdgurwls00@naver.com>
2020-01-30 17:29:05 -0800

    added routes.php

--- app/routes.php
+++ app/routes.php
@@ -10,5 +10,20 @@ $app->get('/sendmail', 'App\Controller\HomeController:sendMail')
 $app->get('/signup', 'App\Controller\HomeController:signup')
     ->setName('signup');
 
+$app->post('/signin', 'App\Controller\HomeController:signin')
+    ->setName('signin');
+
+$app->get('/testJSON', 'App\Controller\HomeController:testJSON')
+    ->setName('testJSON');
+
+$app->get('/testQuery', 'App\Controller\HomeController:testQuery')
+    ->setName('testQuery');
+
+$app->post('/longerpate', 'App\Controller\HomeController:longerpath')
+    ->setName('longerpate');
+
+$app->post('/receiveData', 'App\Controller\HomeController:receiveData')
+    ->setName('receiveData');
+
 $app->get('/post/{id}', 'App\Controller\HomeController:viewPost')
     ->setName('view_post');
